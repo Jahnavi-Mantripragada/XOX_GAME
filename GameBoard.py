9ea485dacd6b9fea3d45579fdee5efeb74d316d3
Jahnavi_M <jahnaviblue@gmail.com>
2021-08-30 16:31:34 +0530

    Converted GameBoard into a Class

--- GameBoard.py
+++ GameBoard.py
@@ -13,148 +13,141 @@ DISABLED_FOREGROUND = "disabledforeground"
 FOREGROUND = "fg"
 X, O = "X", "O"
 
-curr_player = [X]
-pressed = [0]
 
-# The properties of the cell change on a press according to the player who pressed it.
-players = {
-    X: {DISABLED_FOREGROUND: "blue", BACKGROUND: "#87CEEB", STATE: DISABLED, TEXT: X},
-    O: {DISABLED_FOREGROUND: "red", BACKGROUND: "#FFCCCB", STATE: DISABLED, TEXT: O}
-}
+class GameBoard(Tk):
 
-# When we want to reset the board.
-resetting = {
-    FOREGROUND: "grey", BACKGROUND: "#C0C0C0", STATE: NORMAL, TEXT: ""
-}
+    def __init__(self):
+        super().__init__()
 
+        # This frame holds the cells of the board. (Buttons)
+        self.f = Frame(self, width=50, height=50)
+        # The Place where we display the verdict or tell its X's or O's Turn.
+        self.AnnouncementLabel = Label(self, padx=20)
+        self.title("XOX GAME!")
+        self.curr_player = X
+        self.pressed = 0
+        # The Board:
+        self.board = []
+        # We are using a frame to get the two buttons, reLabel, Quit side-by-side.
+        self.top = Frame(self)
+        self.reLabel = Button(self.top, text="Reset", padx=40, pady=20, command=self.resetAllCells, bg="#90EE90", bd=10)
+        self.Quit = Button(self.top, text="Quit", padx=40, pady=20, command=self.destroy, bg="#dc143c", bd=10)
 
-# Before we start a new Game.
-def initialState():
-    curr_player[0] = X
-    pressed[0] = 0
-    AnnouncementLabel["text"] = "X's Turn"
+        # The properties of the cell change on a press according to the player who pressed it.
+        self.players = {
+            X: {DISABLED_FOREGROUND: "blue", BACKGROUND: "#87CEEB", STATE: DISABLED, TEXT: X},
+            O: {DISABLED_FOREGROUND: "red", BACKGROUND: "#FFCCCB", STATE: DISABLED, TEXT: O}
+        }
 
+        # When we want to reset the board.
+        self.resetting = {
+            FOREGROUND: "grey", BACKGROUND: "#C0C0C0", STATE: NORMAL, TEXT: ""
+        }
+        self.setWidgets()
+        self.resizable(False, False)
 
-# Since this is a two-player game, and there would alternate chances.
-def changePlayer():
-    curr_player[0] = X if curr_player[0] == O else O
+    def setWidgets(self):
+        self.AnnouncementLabel.pack()
+        self.AnnouncementLabel.config(font=('Helvetica bold', 20))
 
+        # Initialization for a new game.
+        self.initialState()
 
-# Creating widgets: buttons! as a board.
-def buildBoard():
-    for i in range(3):
-        rows = []
-        for j in range(3):
-            b = Button(f, text="", padx=40, pady=20, command=partial(move, i, j), bg="#C0C0C0", bd=10)
-            b.grid(row=i + 1, column=j + 1)
-            b.config(width=10, height=2)
-            rows.append(b)
-        board.append(rows)
+        self.buildBoard()
+        self.f.pack()
 
+        self.top.pack(side=TOP)
 
-# After a game ends (either a win or draw), we disable all the cells.
-def disableAllCells():
-    for i in range(3):
-        for j in range(3):
-            board[i][j][STATE] = DISABLED
+        self.reLabel.config(font=('Helvetica bold', 15))
+        self.Quit.config(font=('Helvetica bold', 15))
 
+        self.reLabel.pack(in_=self.top, side=LEFT)
+        self.Quit.pack(in_=self.top, side=LEFT)
 
-# When we reset the board, or we are getting ready for a rematch.
-def resetAllCells():
-    initialState()
-    reLabel["text"] = "Reset"
-    for i in range(3):
-        for j in range(3):
-            for reset_options in resetting:
-                board[i][j][reset_options] = resetting[reset_options]
+    # Before we start a new Game.
+    def initialState(self):
+        self.curr_player = X
+        self.pressed = 0
+        self.AnnouncementLabel["text"] = "X's Turn"
 
+    # Creating widgets: buttons! as a board.
+    def buildBoard(self):
+        for i in range(3):
+            rows = []
+            for j in range(3):
+                b = Button(self.f, text="", padx=40, pady=20, command=partial(self.move, i, j), bg="#C0C0C0", bd=10)
+                b.grid(row=i + 1, column=j + 1)
+                b.config(width=10, height=2)
+                rows.append(b)
+            self.board.append(rows)
 
-# We keep checking the board after every turn, so, we can find if a player won.
-def checkIfAPlayerWon(i, j):
-    player = curr_player[0]
-    # check the row
-    for r in range(3):
-        if board[r][j][TEXT] != player:
-            break
-    else:
-        return True
+    # Since this is a two-player game, and there would alternate chances.
+    def changePlayer(self):
+        self.curr_player = X if self.curr_player == O else O
 
-    # check the column if current player won.
-    for c in range(3):
-        if board[i][c][TEXT] != player:
-            break
-    else:
-        return True
+    # After a game ends (either a win or draw), we disable all the cells.
+    def disableAllCells(self):
+        for i in range(3):
+            for j in range(3):
+                self.board[i][j][STATE] = DISABLED
 
-    for r, c in zip(range(3), range(3)):
-        if board[r][c][TEXT] != player:
-            break
-    else:
-        return True
+    # When we reset the board, or we are getting ready for a rematch.
+    def resetAllCells(self):
+        self.initialState()
+        self.reLabel["text"] = "Reset"
+        for i in range(3):
+            for j in range(3):
+                for reset_options in self.resetting:
+                    self.board[i][j][reset_options] = self.resetting[reset_options]
 
-    for r, c in zip(range(3), range(2, -1, -1)):
-        if board[r][c][TEXT] != player:
-            break
-    else:
-        return True
-    return False
+    # We keep checking the board after every turn, so, we can find if a player won.
+    def checkIfAPlayerWon(self, i, j):
+        player = self.curr_player
+        # check the row
+        for r in range(3):
+            if self.board[r][j][TEXT] != player:
+                break
+        else:
+            return True
 
+        # check the column if current player won.
+        for c in range(3):
+            if self.board[i][c][TEXT] != player:
+                break
+        else:
+            return True
 
-# Move here is to imply each turn played.
-def move(i, j):
-    player = curr_player[0]
-    pressed[0] += 1
-    for values in players[player]:
-        board[i][j][values] = players[player][values]
-    if checkIfAPlayerWon(i, j):
-        winLabel = "{} WINS!".format(curr_player[0])
-        AnnouncementLabel["text"] = winLabel
-        disableAllCells()
-        rematch = "Rematch?"
-        reLabel["text"] = rematch
-    elif pressed[0] == 9:
-        TieLabel = "That's a Draw!"
-        AnnouncementLabel["text"] = TieLabel
-        disableAllCells()
-        rematch = "Rematch?"
-        reLabel["text"] = rematch
-    else:
-        changePlayer()
-        AnnouncementLabel["text"] = "{}'s Turn".format(curr_player[0])
+        for r, c in zip(range(3), range(3)):
+            if self.board[r][c][TEXT] != player:
+                break
+        else:
+            return True
 
+        for r, c in zip(range(3), range(2, -1, -1)):
+            if self.board[r][c][TEXT] != player:
+                break
+        else:
+            return True
+        return False
 
-# The Board:
-board = []
-
-# The Window:
-root = Tk()
-root.title("XOX GAME!")
-
-# The Place where we display the verdict or tell its X's or O's Turn.
-AnnouncementLabel = Label(root, padx=20)
-AnnouncementLabel.pack()
-AnnouncementLabel.config(font=('Helvetica bold', 20))
-
-# Initialization for a new game.
-initialState()
-
-# This frame holds the cells of the board. (Buttons)
-f = Frame(root, width=50, height=50)
-buildBoard()
-f.pack()
-
-# We are using a frame to get the two buttons, reLabel, Quit side-by-side.
-top = Frame(root)
-top.pack(side=TOP)
-
-reLabel = Button(root, text="Reset", padx=40, pady=20, command=resetAllCells, bg="#90EE90", bd=10)
-Quit = Button(root, text="Quit", padx=40, pady=20, command=root.destroy, bg="#dc143c", bd=10)
-
-reLabel.config(font=('Helvetica bold', 15))
-Quit.config(font=('Helvetica bold', 15))
-
-reLabel.pack(in_=top, side=LEFT)
-Quit.pack(in_=top, side=LEFT)
-
-root.resizable(False, False)
-root.mainloop()
+    # Move here is to imply each turn played.
+    def move(self, i, j):
+        player = self.curr_player
+        self.pressed += 1
+        for values in self.players[player]:
+            self.board[i][j][values] = self.players[player][values]
+        if self.checkIfAPlayerWon(i, j):
+            winLabel = "{} WINS!".format(self.curr_player)
+            self.AnnouncementLabel["text"] = winLabel
+            self.disableAllCells()
+            rematch = "Rematch?"
+            self.reLabel["text"] = rematch
+        elif self.pressed == 9:
+            TieLabel = "That's a Draw!"
+            self.AnnouncementLabel["text"] = TieLabel
+            self.disableAllCells()
+            rematch = "Rematch?"
+            self.reLabel["text"] = rematch
+        else:
+            self.changePlayer()
+            self.AnnouncementLabel["text"] = "{}'s Turn".format(self.curr_player)
